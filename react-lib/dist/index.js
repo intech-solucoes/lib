a00594d5c34ebb55179ecf482660d1a0da655612
Rony Moura <ronymmoura@gmail.com>
2018-06-26 18:31:00 -0300

    Ajustes para a primeira versão

---
+++ react-lib/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.handleFieldChange = undefined;
+
+var _handleFieldChange = require("./forms/handleFieldChange");
+
+var _handleFieldChange2 = _interopRequireDefault(_handleFieldChange);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.handleFieldChange = _handleFieldChange2.default;
