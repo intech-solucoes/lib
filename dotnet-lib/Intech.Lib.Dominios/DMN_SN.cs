2d2c737d070bbf5e2d0375828359059d5a432cf6
Rony Moura <ronymmoura@gmail.com>
2018-09-06 11:35:34 -0300

    Criando projeto de domínios

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_SN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_SN
+    {
+        public const string SIM = "SIM";
+        public const string NAO = "NAO";
+    }
+}
