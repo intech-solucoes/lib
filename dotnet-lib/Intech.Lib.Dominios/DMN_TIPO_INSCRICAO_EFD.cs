6ca92d925621c60bd79857c8c2fda785c5a55230
Rony Moura <ronymmoura@gmail.com>
2018-09-14 16:42:14 -0300

    Criando DMN_TIPO_INSCRICAO_EFD

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_TIPO_INSCRICAO_EFD.cs
@@ -0,0 +1,8 @@
+﻿namespace Intech.Lib.Dominios
+{
+    public class DMN_TIPO_INSCRICAO_EFD
+    {
+        public const string PESSOA_JURIDICA = "1";
+        public const string PESSOA_FISICA = "2";
+    }
+}
