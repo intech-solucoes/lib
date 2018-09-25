7b378aaea56cef975022cbf123269d77268666c3
Rony Moura <ronymmoura@gmail.com>
2018-09-25 10:50:26 -0300

    Melhorando função handleFieldChange

--- react-lib/forms/handleFieldChange.js
+++ react-lib/forms/handleFieldChange.js
@@ -1,10 +1,19 @@
-function handleFieldChange(context, event) {
+function handleFieldChange(context, event, parent = null) {
     var name = event.target.name;
     var value = event.target.value;
 
-    context.setState({
-        [name]: value
-    });
+    if(parent) {
+        var parentObj = context.state[parent];
+        parentObj[name] = value;
+
+        context.setState({
+            [parent]: parentObj
+        });
+    } else {
+        context.setState({
+            [name]: value
+        });
+    }
 }
 
 export default handleFieldChange;
