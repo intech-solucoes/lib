e76100c5d7c2cf527b0cf78a7a399184562474d2
Rony Moura <ronymmoura@gmail.com>
2018-09-27 11:56:13 -0300

    Criando domínio DMN_EFR_EFD

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_EFR_EFD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_EFR_EFD
+    {
+        public const string SIM = "S";
+        public const string NAO = "N";
+    }
+}
