41d5260dcfe3d258a88a93548e0a8f05a8ee4375
andre <andre.felippi@intech.com.br>
2018-10-03 15:07:22 -0300

    Criação do domínio 'DMN_STATUS_EFD_UPLOAD'

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_STATUS_EFD_UPLOAD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_STATUS_EFD_UPLOAD
+    {
+        public const string NAO_PROCESSADO = "NAO";
+        public const string PROCESSADO = "PRO";
+    }
+}
