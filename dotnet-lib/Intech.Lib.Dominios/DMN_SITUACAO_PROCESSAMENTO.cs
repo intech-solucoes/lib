adfd861bf8a4d4591ded9393b714be869556fc79
andre <andre.felippi@intech.com.br>
2018-10-04 09:30:44 -0300

    Criação do domínio 'DMN_SITUACAO_PROCESSAMENTO'

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_SITUACAO_PROCESSAMENTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_SITUACAO_PROCESSAMENTO
+    {
+        public const string IMPORTADO = "IMP";
+        public const string PROCESSADO = "PRO";
+        public const string ENVIADO = "ENV";
+        public const string RETIFICADO = "RET";
+    }
+}
