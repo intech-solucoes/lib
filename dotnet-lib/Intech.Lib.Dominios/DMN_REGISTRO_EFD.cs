54f8eab0e6b6b1867bbc74cfce987f9ce4701372
andre <andre.felippi@intech.com.br>
2018-10-04 11:18:08 -0300

    Criação do domínio 'DMN_REGISTRO_EFD'

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_REGISTRO_EFD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_REGISTRO_EFD
+    {
+        public const string R_1070 = "R-1070";
+        public const string R_2010 = "R-2010";
+    }
+}
