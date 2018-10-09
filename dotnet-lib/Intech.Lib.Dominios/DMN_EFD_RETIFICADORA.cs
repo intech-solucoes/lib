43edd0507ceeece511f62282e3c2c11e96951d53
andre <andre.felippi@intech.com.br>
2018-10-09 14:17:32 -0300

    Criação do domínio 'DMN_EFD_RETIFICADORA'

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_EFD_RETIFICADORA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_EFD_RETIFICADORA
+    {
+        public const string ORIGINAL = "1";
+        public const string RETIFICADORA = "2";
+    }
+}
