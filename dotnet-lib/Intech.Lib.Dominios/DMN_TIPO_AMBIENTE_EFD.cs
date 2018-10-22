0b8e095680e364076b74895655bd6e0133ecc952
Rony Moura <ronymmoura@gmail.com>
2018-10-22 16:53:01 -0300

    Criação do domínio DMN_TIPO_AMBIENTE_EFD

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_TIPO_AMBIENTE_EFD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_TIPO_AMBIENTE_EFD
+    {
+        public const string PRODUCAO = "1";
+        public const string PRODUCAO_RESTRITA = "2";
+    }
+}
