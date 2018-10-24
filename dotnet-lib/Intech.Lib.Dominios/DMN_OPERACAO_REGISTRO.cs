5f47bb123aed7613dc677e06c3b5e1355777e202
andre <andre.felippi@intech.com.br>
2018-10-24 16:19:30 -0300

    Criação do domínio DMN_OPERACAO_REGISTRO

---
+++ dotnet-lib/Intech.Lib.Dominios/DMN_OPERACAO_REGISTRO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intech.Lib.Dominios
+{
+    public static class DMN_OPERACAO_REGISTRO
+    {
+        public const string INCLUSAO = "INC";
+        public const string ALTERACAO = "ALT";
+        public const string EXCLUSAO = "EXC";
+    }
+}
