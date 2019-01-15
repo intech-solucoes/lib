f22a5c7086fd075b5db49746f6d1094e0dae7d3a
Rony Moura <ronymmoura@gmail.com>
2019-01-15 19:31:52 -0200

    Removendo service

--- react-lib/index.js
+++ react-lib/index.js
@@ -1,10 +1,7 @@
 import handleFieldChange from "./forms/handleFieldChange";
 import { validarEmail } from "./forms/Validacoes";
-import BaseService from "./service/BaseService";
 
 export { 
     handleFieldChange,
-    validarEmail,
-
-    BaseService
+    validarEmail
 };
