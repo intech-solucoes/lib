184d55bdc81727a6afde18e46bddbc0357264707
Luis Bruno <l_brunofidelis@yahoo.com.br>
2019-02-14 22:07:22 -0200

    Ajustando handleFieldChange para checkbox

--- react-lib/forms/handleFieldChange.js
+++ react-lib/forms/handleFieldChange.js
@@ -2,7 +2,7 @@ function handleFieldChange(context, event) {
     var parent = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
 
     var name = event.target.name;
-    var value = event.target.value;
+    var value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
 
     if(parent) {
         var parentObj = context.state[parent];
